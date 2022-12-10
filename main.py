cb418960f804c88c4c26e69d0390fc06bbd2d4fb
Jared.Tan <Jtanu45@gmail.com>
2022-12-10 04:06:01 -0800

    Fully implemented backwards elimination and added time calculations

--- main.py
+++ main.py
@@ -78,7 +78,33 @@ def leave_one_out_cross_validation(data, current_set_of_features, feature_to_add
         if label_object_to_classify == nearest_neighbor_label:
             number_correctly_classified = number_correctly_classified + 1
         #print(nearest_neighbor_distance, "\n")
-    print(number_correctly_classified/data.shape[0])
+    return number_correctly_classified / data.shape[0]
+
+def leave_one_out_cross_validation_backwards(data, current_set_of_features, feature_to_remove):
+    number_correctly_classified = 0
+    
+    features = current_set_of_features
+    features = np.insert(features, 0, 0, 0)
+    features = np.delete(features, np.where(features == feature_to_remove))
+    tempdata = data[:,features]
+    
+    for i in range(0, data.shape[0]):
+        object_to_classify = tempdata[i,1:]
+        label_object_to_classify = tempdata[i,0]
+        
+        nearest_neighbor_distance = np.inf
+        nearest_neighbor_location = np.inf
+        
+        for k in range(0, tempdata.shape[0]):
+            if k != i:
+                temp = np.subtract(object_to_classify, tempdata[k,1:])
+                distance = math.sqrt(np.dot(temp, temp))
+                if distance < nearest_neighbor_distance:
+                    nearest_neighbor_distance = distance
+                    nearest_neighbor_location = k
+                    nearest_neighbor_label = tempdata[nearest_neighbor_location,0]
+        if label_object_to_classify == nearest_neighbor_label:
+            number_correctly_classified = number_correctly_classified + 1
     return number_correctly_classified / data.shape[0]
 
 def forward_select(data):
@@ -95,44 +121,45 @@ def forward_select(data):
             if (k in current_set_of_features):
                 continue
             else:
-                #print("-- Considering adding the "+str(k)+" feature")
                 accuracy = leave_one_out_cross_validation(data, current_set_of_features, k)
+                print("    Using feature(s) ", current_set_of_features, " and considering addding feature ", k, " Accuracy is ", round(accuracy*100,2),"%\n")
                 if accuracy > best_acc_so_far:
                     best_acc_so_far = accuracy
                     feature_to_add_at_this_level = k
 
         if feature_to_add_at_this_level:
             current_set_of_features.append(feature_to_add_at_this_level)
-        print("On level "+str(i+1)+" I added feature "+str(feature_to_add_at_this_level)+" to current set")
+        print("On level "+str(i+1)+" I added feature "+str(feature_to_add_at_this_level)+" to current set.\n")
+        print("Best feature set was ", current_set_of_features, ", accuracy is ", round(best_acc_so_far*100,2),"%\n")
     endtime = time.time()
     totaltime = round(endtime - starttime,2)
     print("Finished search! The best feature set is ",current_set_of_features)
-    print(" It has an accuracy of ", best_acc_so_far)
+    print("It has an accuracy of ", round(best_acc_so_far*100,2), "% And took ", totaltime, "seconds.")
 
 def backward_elim(data):
-    current_set_of_features = [] #Initialize empty set
+    starttime = time.time()
+    current_set_of_features = np.arange(1,data.shape[1]) #Initialize full set
     
     best_acc_so_far = 0
 
     for i in range(0, data.shape[1]-1):
         print("On level "+str(i+1)+" of the search tree")
-        feature_to_add_at_this_level = None
+        feature_to_remove_at_this_level = None
         
         for k in range(1, data.shape[1]):
-            if (k in current_set_of_features):
-                continue
-            else:
-                #print("-- Considering adding the "+str(k)+" feature")
-                accuracy = leave_one_out_cross_validation(data, current_set_of_features, k)
-                if accuracy > best_acc_so_far:
-                    best_acc_so_far = accuracy
-                    feature_to_add_at_this_level = k
+            accuracy = leave_one_out_cross_validation_backwards(data, current_set_of_features, k)
+            print("    Using feature(s) ", current_set_of_features, " and considering removing feature ", k, " Accuracy is ", round(accuracy*100,2),"%\n")
+            if accuracy > best_acc_so_far:
+                best_acc_so_far = accuracy
+                feature_to_remove_at_this_level = k
 
-        if feature_to_add_at_this_level:
-            current_set_of_features.append(feature_to_add_at_this_level)
-        print("On level "+str(i+1)+" I added feature "+str(feature_to_add_at_this_level)+" to current set")
+        if feature_to_remove_at_this_level:
+            current_set_of_features = np.delete(current_set_of_features, np.where(current_set_of_features == feature_to_remove_at_this_level))
+        print("On level ",str(i+1)," I removed feature ",str(feature_to_remove_at_this_level)," from the current set")
+    endtime = time.time()
+    totaltime = round(endtime - starttime,2)
     print("Finished search! The best feature set is ",current_set_of_features)
-    print(" It has an accuracy of ", best_acc_so_far)
+    print(" It has an accuracy of ", round(best_acc_so_far*100,2), "% And took ", totaltime, "seconds.")
 
 if __name__ == "__main__":
     main()
